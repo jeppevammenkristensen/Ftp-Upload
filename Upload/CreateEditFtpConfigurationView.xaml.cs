76bb54adc2235a87992e451436494ac971d952d4
Jeppe Vammen Kristensen <jeppe@jeppek.dk>
2015-02-19 21:02:29 +0100

    Added windows for configering FTP

---
+++ Upload/CreateEditFtpConfigurationView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Upload.ViewModels;
+
+namespace Upload
+{
+    /// <summary>
+    /// Interaction logic for CreateEditFtpConfigurationView.xaml
+    /// </summary>
+    public partial class CreateEditFtpConfigurationView : Window
+    {
+        public CreateEditFtpConfigurationViewModel Scope { get; set; }
+
+        public CreateEditFtpConfigurationView()
+        {
+            InitializeComponent();
+
+        }
+
+        protected async override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+            Scope = new CreateEditFtpConfigurationViewModel();
+            DataContext = Scope;
+        }
+
+        
+    }
+}
